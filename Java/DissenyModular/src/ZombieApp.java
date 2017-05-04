1958e1cc218ca5e0abcc00ba3a3e232f3830e6a4
Hector Blanco <hectorblancofelipe@gmail.com>
2017-05-04 01:27:27 +0000

    commit16

--- Java/DissenyModular/src/ZombieApp.java
+++ Java/DissenyModular/src/ZombieApp.java
@@ -1,8 +1,3 @@
-public class ElectroApp {
+public class ZombieApp {
 
-	public static void main (String[] args){
-		
-		Electro e = new Electro(150, "negro", 'G', 30);
-		System.out.println("Precio final del electrodomestico de color "+e.getColor()+": "+e.precioFinal()+" €");
-	}
 }
